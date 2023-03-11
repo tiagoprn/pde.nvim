c5be39643dc60ef9d56b2066d7b0b674dc646425
tiago.paranhos <tiagoprn@gmail.com>
2023-03-11 07:19:05 -0300

    feat(which-key): defined the leader groups for the new mappings.

--- lua/which-key-conf.lua
+++ lua/which-key-conf.lua
@@ -10,10 +10,59 @@ which_key.setup({})
 which_key.register({
 	["<leader>"] = {
 		v = {
-			name = "+custom LEADER group",
+			name = "+sample LEADER group",
 			f = { "<cmd>Telescope find_files<cr>", "LEADER Find File" },
 			r = { "<cmd>Telescope oldfiles<cr>", "LEADER Open Recent File" },
 			n = { "<cmd>enew<cr>", "LEADER New File" },
+		},
+		--
+		l = {
+			name = "+lazy",
+		},
+		s = {
+			name = "+session",
+		},
+		f = {
+			name = "+files",
+		},
+		g = {
+			name = "+grep",
+		},
+		t = {
+			name = "+tree",
+		},
+		T = {
+			name = "+toggle",
+		},
+		w = {
+			name = "+writing",
+		},
+		c = {
+			name = "+coding",
+		},
+		b = {
+			name = "+clipboard",
+		},
+		q = {
+			name = "+quickfix",
+		},
+		m = {
+			name = "+messages / notifications",
+		},
+		o = {
+			name = "+formatting",
+		},
+		h = {
+			name = "+harpoon",
+		},
+		x = {
+			name = "+external commands",
+		},
+		p = {
+			name = "+python",
+		},
+		e = {
+			name = "+etc",
 		},
 	},
 })
