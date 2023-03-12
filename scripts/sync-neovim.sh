5fbb5a78e9e0366166e6e8106b6156c9e5b99641
tiagoprn <tiagoprn@gmail.com>
2023-03-12 08:08:49 +0000

    Bringing other helper nvim scripts to here.

---
+++ scripts/sync-neovim.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+: '
+This script (re)compiles neovim from its official repository.
+'
+
+# Cleanup existing install and compile a new one
+PREVIOUS_VERSION=$(sudo -- bash -c 'cd /opt/src/neovim && git log -n 1 --pretty=format:"%cD by %an (%h)"')
+
+BACKUPS_ROOT=/opt/src/neovim/tmp/OLD-VERSIONS-ARCHIVE
+SUFFIX="$(date +%Y%m%d-%H%M%S-%N)"
+BACKUPS_DIR=$BACKUPS_ROOT/$SUFFIX
+NVIM_BINARY_PATH=/usr/local/bin/nvim
+
+sudo mkdir -p $BACKUPS_DIR
+sudo cp -farv $NVIM_BINARY_PATH $BACKUPS_DIR
+echo "The old nvim binary was copied to $BACKUPS_DIR in case you need to manually revert it to $NVIM_BINARY_PATH."
+
+echo "Compiling nvim (this will take a while)..."
+echo -e "If the build fails because of old version of libraries,\n run the script 'nvim-clean-cmake-build-cache.sh'\n to delete cmake cache and rebuild from a pristine state."
+read -n 1 -s -r -p "Press any key to continue..."
+COMMANDS="cd /opt/src/neovim && git fetch && git pull && rm -fr /opt/src/neovim/build && make clean && make CMAKE_BUILD_TYPE=Release && make install"
+sudo -- bash -c "$COMMANDS"
+
+NEW_VERSION=$(sudo -- bash -c 'cd /opt/src/neovim && git log -n 1 --pretty=format:"%cD by %an (%h)"')
+
+message="\nFinished compiling nvim.\n\nPrevious version:\n\t$PREVIOUS_VERSION\nNew version:\n\t$NEW_VERSION\n\nHave fun! \o/"
+echo "-----------------"
+echo -e "$message"
+echo "-----------------"
