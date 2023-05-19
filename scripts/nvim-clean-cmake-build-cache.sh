fe1a71c9d416384978ac28c3192eb666290c306d
tiago.paranhos <tiagoprn@gmail.com>
2023-05-19 08:33:34 -0300

    feat(scripts): updated the clean script.

--- scripts/nvim-clean-cmake-build-cache.sh
+++ scripts/nvim-clean-cmake-build-cache.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# sudo rm -fr /opt/src/neovim/build
-echo -e "If that did not work, run: \n cd /opt/src/neovim && ninja -d explain -C build"
+TIMESTAMP="$(date "+%Y%m%d.%H%M.%S")"
+
+echo "Moving current instalattion directory..."
+sudo mv /opt/src/neovim /opt/src/neovim."$TIMESTAMP"
+
+echo "Cloning neovim repository again..."
+sudo bash -c "cd /opt/src && git clone https://github.com/neovim/neovim.git"
+
+echo -e "FINISHED! \n Now you can manually run './sync-neovim.sh' again. "
