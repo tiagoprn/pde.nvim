7454359857683bd8db494b62ee44d96c9542f687
tiago.paranhos <tiagoprn@gmail.com>
2023-09-21 07:36:37 -0300

    feat(lua): updated lua configuration including the LSP server, now fully functional.

--- lua/lua-lsp.lua
+++ lua/lua-lsp.lua
@@ -5,8 +5,8 @@ require("neodev").setup({
 })
 
 -- set the path to the lua_ls installation
--- local lua_ls_root_path = "/opt/src/lua-language-server"
--- local lua_ls_binary = lua_ls_root_path .. "/bin/lua-language-server"
+local lua_ls_root_path = vim.fn.expand("~/.config/nvim/lua-language-server")
+local lua_ls_binary = lua_ls_root_path .. "/bin/lua-language-server"
 
 -- local runtime_path = vim.split(package.path, ";")
 -- table.insert(runtime_path, "lua/?.lua")
@@ -14,29 +14,34 @@ require("neodev").setup({
 
 local lspconfig = require("lspconfig")
 lspconfig.lua_ls.setup({
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-				-- Setup your lua path
-				-- path = runtime_path,
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			-- completion = {
-			-- 	callSnippet = "Replace",
-			-- },
-			-- workspace = {
-			--   Make the server aware of Neovim runtime files
-			--   library = vim.api.nvim_get_runtime_file("", true),
-			-- },
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+	cmd = { lua_ls_binary, "-E", lua_ls_root_path .. "/main.lua" },
+	-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+	on_init = function(client)
+		local path = client.workspace_folders[1].name
+		if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+			client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+				Lua = {
+					runtime = {
+						-- Tell the language server which version of Lua you're using
+						-- (most likely LuaJIT in the case of Neovim)
+						version = "LuaJIT",
+					},
+					-- Make the server aware of Neovim runtime files
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+							-- "${3rd}/luv/library"
+							-- "${3rd}/busted/library",
+						},
+						-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+						-- library = vim.api.nvim_get_runtime_file("", true)
+					},
+				},
+			})
+
+			client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+		end
+		return true
+	end,
 })
