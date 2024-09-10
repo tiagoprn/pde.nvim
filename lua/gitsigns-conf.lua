dddcb5d7ed3454451e9eb92890345e4ed7b0053b
tiago.paranhos <tiagoprn@gmail.com>
2024-09-10 16:08:15 -0300

    feat(plugins): fixing minor configuration issues

--- lua/gitsigns-conf.lua
+++ lua/gitsigns-conf.lua
@@ -29,9 +29,7 @@ gitsigns.setup({
 		delay = 1000,
 		ignore_whitespace = false,
 	},
-	current_line_blame_formatter_opts = {
-		relative_time = false,
-	},
+	current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
@@ -43,8 +41,5 @@ gitsigns.setup({
 		relative = "cursor",
 		row = 0,
 		col = 1,
-	},
-	yadm = {
-		enable = false,
 	},
 })
