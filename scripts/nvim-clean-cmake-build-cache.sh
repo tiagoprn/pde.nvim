9f9cd9813c60c11987bc87e7593a69ab53795221
tiago.paranhos <tiagoprn@gmail.com>
2024-10-09 08:32:18 -0300

    fix(scripts): updated

--- scripts/nvim-clean-cmake-build-cache.sh
+++ scripts/nvim-clean-cmake-build-cache.sh
@@ -2,10 +2,12 @@
 
 TIMESTAMP="$(date "+%Y%m%d.%H%M.%S")"
 
+ROOT_SOURCES_DIR="/opt/src"
+
 echo "Moving current instalattion directory..."
-sudo mv /storage/vendors/neovim /storage/vendors/neovim."$TIMESTAMP"
+sudo mv $ROOT_SOURCES_DIR/neovim $ROOT_SOURCES_DIR/neovim."$TIMESTAMP"
 
 echo "Cloning neovim repository again..."
-sudo bash -c "cd /storage/vendors && git clone https://github.com/neovim/neovim.git"
+sudo bash -c "cd $ROOT_SOURCES_DIR && git clone https://github.com/neovim/neovim.git"
 
 echo -e "FINISHED! \n Now you can manually run './sync-neovim.sh' again. "
