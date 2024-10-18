2fcb7f62a49488a0782e659f41b87382b0a5598e
tiago.paranhos <tiagoprn@gmail.com>
2024-10-17 21:23:10 -0300

    feat(plugins): added ChatGPT.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -212,6 +212,41 @@ require("lazy").setup({
     },
   },
 
+  -- ChatGPT
+  {
+    "jackMort/ChatGPT.nvim",
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "pass api-keys/OPENAI",
+        openai_params = {
+          -- NOTE: model can be a function returning the model name
+          -- this is useful if you want to change the model on the fly
+          -- using commands
+          -- Example:
+          -- model = function()
+          --     if some_condition() then
+          --         return "gpt-4-1106-preview"
+          --     else
+          --         return "gpt-3.5-turbo"
+          --     end
+          -- end,
+          model = "gpt-4o", -- check navi command search "chatgpt" to get all available models according to their API
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 4095,
+          temperature = 0.2,
+          top_p = 0.1,
+          n = 1,
+        },
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+
   -- LANGUAGE SERVERS - begin
   -- Handles automatically launching and initializing language servers installed on your system
   { "neovim/nvim-lspconfig" },
