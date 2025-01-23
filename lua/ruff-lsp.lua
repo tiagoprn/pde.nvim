f3e16ff36470edd5c25d21621e666d79865d2687
tiago.paranhos <tiagoprn@gmail.com>
2025-01-23 11:16:03 +0000

    feat: (wip) configuring ruff

--- lua/ruff-lsp.lua
+++ lua/ruff-lsp.lua
@@ -1,22 +1,22 @@
 local lsp = require("lspconfig")
-local helpers = require("tiagoprn.helpers")
+
+vim.lsp.set_log_level("debug")
 
 lsp.ruff.setup({
   capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  cmd = {
-    vim.fn.getenv("HOME") .. "/.pyenv/versions/neovim/bin/ruff-lsp",
-    -- .. " --config="
-    -- .. helpers.get_pyproject_toml_path(),
-  },
+  cmd = { vim.fn.getenv("HOME") .. "/.pyenv/versions/neovim/bin/ruff", "server" },
   on_attach = function(client, bufnr)
     client.server_capabilities.document_formatting = false
-    client.server_capabilities.document_range_formatting = false
+    client.server_capabilities.document_range_formatting = true
     client.server_capabilities.document_diagnostics = true
   end,
   init_options = {
     settings = {
       -- Any extra CLI arguments for `ruff` go here.
+      logLevel = "debug",
+      logFile = vim.fn.getenv("HOME") .. "/.local/state/nvim/ruff.log",
       args = {},
     },
   },
+  single_file_support = true,
 })
