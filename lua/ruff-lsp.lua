d1a77fa4fd77056dad69a0f72c87c07d9cf7ca0d
tiago.paranhos <tiagoprn@gmail.com>
2025-01-23 11:40:20 +0000

    feat(ruff-lsp): finishing configuration.

--- lua/ruff-lsp.lua
+++ lua/ruff-lsp.lua
@@ -1,6 +1,8 @@
 local lsp = require("lspconfig")
 
-vim.lsp.set_log_level("debug")
+-- below changes the log level of lsp, to make it more verbose so can I check its' communication with neovim.
+-- The lsp log file is at ~/.local/state/nvim/lsp.log
+vim.lsp.set_log_level("debug") -- change to "debug" for troubleshooting
 
 lsp.ruff.setup({
   capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
@@ -9,10 +11,44 @@ lsp.ruff.setup({
     client.server_capabilities.document_formatting = false
     client.server_capabilities.document_range_formatting = true
     client.server_capabilities.document_diagnostics = true
+
+    -- Request diagnostics when I open the file (and do other buffer/window/tab events) explicitly.
+    -- Otherwise it does not show after I open it, just if a reload it.
+    vim.api.nvim_create_autocmd({
+      "BufEnter",
+      "BufWritePost",
+      "WinEnter",
+      "WinNew",
+      "BufWinEnter",
+      "TabEnter",
+      "BufReadPost",
+    }, {
+      buffer = bufnr,
+      callback = function()
+        vim.diagnostic.enable(bufnr)
+        client.request("textDocument/diagnostic", {
+          textDocument = vim.lsp.util.make_text_document_params(bufnr),
+        }, nil, bufnr)
+      end,
+    })
+
+    -- Optional: Show diagnostics in a floating window on cursor hover
+    vim.api.nvim_create_autocmd("CursorHold", {
+      buffer = bufnr,
+      callback = function()
+        vim.diagnostic.open_float(nil, {
+          focusable = false,
+          close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+          source = "always",
+          scope = "cursor",
+        })
+      end,
+    })
   end,
   init_options = {
     settings = {
       -- Any extra CLI arguments for `ruff` go here.
+      -- change "logLevel" to "debug" below for troubleshooting
       logLevel = "debug",
       logFile = vim.fn.getenv("HOME") .. "/.local/state/nvim/ruff.log",
       args = {},
