a33fe0cde8a2a3c781884f6262f964cb84d4eb15
tiago.paranhos <tiagoprn@gmail.com>
2025-03-26 15:19:15 -0300

    feat(snippets): (wip) migrating to luasnip

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -88,7 +88,46 @@ require("lazy").setup({
   { "kylechui/nvim-surround" },
 
   -- Snippets
-  { "dcampos/nvim-snippy" },
+  -- { "dcampos/nvim-snippy" },
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    config = function()
+      local ls = require("luasnip")
+
+      -- Load snippets from ~/.config/nvim/lua/snippets
+      require("luasnip.loaders.from_lua").lazy_load({
+        paths = vim.fn.expand("~/.config/nvim/lua/snippets"),
+      })
+
+      -- Keybindings for jumping in snippets
+      vim.keymap.set({ "i", "s" }, "<Tab>", function()
+        if ls.expand_or_jumpable() then
+          ls.expand_or_jump()
+        else
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", true)
+        end
+      end, { silent = true })
+
+      vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+        if ls.jumpable(-1) then
+          ls.jump(-1)
+        else
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", true)
+        end
+      end, { silent = true })
+
+      -- Optional: cycle through choices with Ctrl+e
+      vim.keymap.set("i", "<C-e>", function()
+        if ls.choice_active() then
+          ls.change_choice(1)
+        end
+      end)
+    end,
+  },
 
   -- Macros persistence
   { "chamindra/marvim" },
@@ -525,8 +564,9 @@ require("lazy").setup({
   -- { "hrsh7th/cmp-path" },
   {
     "saghen/blink.cmp",
-    -- -- optional: provides snippets for the snippet source
-    dependencies = { "onsails/lspkind.nvim" },
+    dependencies = {
+      "onsails/lspkind.nvim",
+    },
 
     -- use a release tag to download pre-built binaries
     version = "1.*",
@@ -617,7 +657,7 @@ require("lazy").setup({
         },
       },
 
-      -- snippets = { preset = "luasnip" },
+      snippets = { preset = "luasnip" },
 
       signature = { enabled = true },
 
