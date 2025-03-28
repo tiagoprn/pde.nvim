27bfa6460da9316c3e4d6e4466f06efd42fa98e4
tiago.paranhos <tiagoprn@gmail.com>
2025-03-28 11:36:31 -0300

    feat(lua): fully functional lua dev env!

--- lua/lua-lsp.lua
+++ lua/lua-lsp.lua
@@ -1,21 +1,7 @@
 local lspconfig = require("lspconfig")
 
--- set the path to the lua_ls installation
--- local lua_ls_root_path = vim.fn.expand("~/.config/nvim/lua-language-server")
--- local lua_ls_binary = lua_ls_root_path .. "/bin/lua-language-server"
-
--- lua language server is super confused when editing lua files in the config
--- and raises a lot of [duplicate-doc-field] warnings
-local runtime_files = vim.api.nvim_get_runtime_file("", true)
-for k, v in ipairs(runtime_files) do
-  if v == vim.fn.expand("~/.config/nvim/after") or v == vim.fn.expand("~/.config/nvim") then
-    table.remove(runtime_files, k)
-  end
-end
-
+-- Configuration for lua_ls (Lua language server)
 lspconfig.lua_ls.setup({
-  -- cmd = { lua_ls_binary, "-E", lua_ls_root_path .. "/main.lua" },
-  -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
   on_init = function(client)
     if client.workspace_folders then
       local path = client.workspace_folders[1].name
@@ -27,26 +13,36 @@ lspconfig.lua_ls.setup({
       end
     end
 
-    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+    -- Apply these settings to fix the "Undefined global 'vim'" error
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua or {}, {
       runtime = {
         -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
         version = "LuaJIT",
       },
       diagnostics = {
-        globals = { "vim" },
+        -- Recognize the `vim` global
+        globals = {
+          "vim",
+          -- Add other globals if needed
+          "awesome",
+          "client",
+          "root",
+        },
       },
-      -- Make the server aware of Neovim runtime files
       workspace = {
-        checkThirdParty = true,
-        -- library = {
-        --   vim.env.VIMRUNTIME,
-        --   -- Depending on the usage, you might want to add additional paths here.
-        --   -- "${3rd}/luv/library"
-        --   -- "${3rd}/busted/library",
-        -- },
-        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-        library = runtime_files,
+        -- Make the server aware of Neovim runtime files
+        library = {
+          -- This is the key part: include the Neovim API in library
+          vim.api.nvim_get_runtime_file("", true),
+          "${3rd}/luv/library",
+        },
+        checkThirdParty = false, -- Disable annoying prompts
+      },
+      telemetry = {
+        enable = false,
+      },
+      completion = {
+        callSnippet = "Replace",
       },
     })
   end,
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -757,43 +757,28 @@ require("lazy").setup({
   { "justinsgithub/wezterm-types" },
   { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
   {
-    -- The plugin below was changed because folke/neodev.nvim is now
-    -- deprecated.
-    -- I still have that old plugin's configuration on "lua-lsp.lua",
-    -- case I need to get any config from there
-    -- (e.g. lua-language-server full path)
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
     opts = {
       library = {
-        -- Library paths can be absolute
-        -- "~/projects/my-awesome-lib",
-        -- Or relative, which means they will be resolved from the plugin dir.
-        "lazy.nvim",
+        -- Include the Neovim runtime APIs
         "luvit-meta/library",
-        -- It can also be a table with trigger words / mods
-        -- Only load luvit types when the `vim.uv` word is found
+        -- Include these standard libraries
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        -- always load the LazyVim library
-        "LazyVim",
-        -- Only load the lazyvim library when the `LazyVim` global is found
-        { path = "LazyVim", words = { "LazyVim" } },
-        -- Load the wezterm types when the `wezterm` module is required
-        -- Needs `justinsgithub/wezterm-types` to be installed
-        { path = "wezterm-types", mods = { "wezterm" } },
+        -- Include your installed plugins
+        "lazy.nvim",
+        -- Automatically load any plugin you're developing
+        -- vim.fn.expand("~/projects/my-plugin"),
       },
+      -- These integrations are important
       integrations = {
+        -- Make sure lspconfig integration is enabled
         lspconfig = true,
+        -- Enable blink.cmp integration if you're using it
+        ["blink.cmp"] = true,
       },
-      -- always enable unless `vim.g.lazydev_enabled = false`
-      -- This is the default
-      enabled = function(root_dir)
-        return vim.g.lazydev_enabled == nil and true or vim.g.lazydev_enabled
-      end,
-      -- disable when a .luarc.json file is found
-      enabled = function(root_dir)
-        return not vim.uv.fs_stat(root_dir .. "/.luarc.json")
-      end,
+      -- This ensures lazydev is always active for Lua files
+      enabled = true,
     },
   },
 
