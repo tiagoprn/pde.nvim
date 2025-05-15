ce53f676553c5ad13a64a781394a0d4dafe78354
tiago.paranhos <tiagoprn@gmail.com>
2025-05-15 05:00:45 -0300

    fix(snippets): using wrong string format

--- lua/snippets/python/traceback_print_current_stack_snippet.lua
+++ lua/snippets/python/traceback_print_current_stack_snippet.lua
@@ -7,11 +7,9 @@ local fmt = require("luasnip.extras.fmt").fmt
 
 local snippet = {
   python = {
-    new_snippet(
+    new_snippet( -- FIXME: error: failed to execute item with error: ....local/share/nvim/lazy/LuaSnip/lua/luasnip/util/util.lua:241: 'replacement string' item contains newlines
       "traceback_print_current_stack",
-      t([[
-__import__('traceback').print_stack()
-]])
+      t("__import__('traceback').print_stack()")
     ),
   },
 }
