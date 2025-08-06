7f6f805c7229ea98b90500b246ad4c6e460e5b39
tiago.paranhos <tiagoprn@gmail.com>
2025-08-06 17:33:57 -0300

    feat(scripts): ignoring lsprotocol package when upgrading the uv venv

--- scripts/update-neovim-uv-virtualenv.sh
+++ scripts/update-neovim-uv-virtualenv.sh
@@ -25,7 +25,9 @@ uv pip freeze >$LOGS_ROOT/pre-update-packages-versions.txt
 echo "Generating list of outdated packages..."
 uv pip list --outdated >$LOGS_ROOT/outdated_packages.txt
 
-# I won't update jedi and jedi-language-server after they are installed.
+# ----- PACKAGES IGNORED FROM UPGRADE - begin
+
+# I won't update jedi, jedi-language-server and lsprotocol after they are installed.
 # When upgrading jedi-language-server from 0.44.0 to 0.45.0 it broke go-to-definitions,
 # so I had to downgrade it manually.
 # So, the code below effectively freezes this package on the current version and do not upgrade it anymore.
@@ -35,6 +37,16 @@ if grep -n '^jedi' "$LOGS_ROOT/outdated_packages.txt"; then
 else
     echo "No lines starting with 'jedi' found."
 fi
+
+# lsprotocol also is not updated frequently by jedi-language-server
+if grep -n '^lsprotocol' "$LOGS_ROOT/outdated_packages.txt"; then
+    echo "Above lines starting with 'lsprotocol' were found and will be removed."
+    sed -i '/^lsprotocol/d' "$LOGS_ROOT/outdated_packages.txt"
+else
+    echo "No lines starting with 'lsprotocol' found."
+fi
+
+# ----- PACKAGES IGNORED FROM UPGRADE - end
 
 # Now check if the file has only 2 lines (header + separator), delete it.
 if [ "$(wc -l <"$LOGS_ROOT/outdated_packages.txt")" -eq 2 ]; then
