ddff686e1f30cba316c45c4cbedb0d7703f873df
tiago.paranhos <tiagoprn@gmail.com>
2026-01-29 14:18:26 -0300

    feat(lsps): updated bash lsp to nvim 0.11 pattern.

--- lua/bash-lsp.lua
+++ lua/bash-lsp.lua
@@ -10,6 +10,7 @@ capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").g
 -- https://github.com/kosayoda/nvim-lightbulb/blob/66223954d7bd7d4358c36d157c25503168d04319/lua/nvim-lightbulb.lua#L195-L201
 -- https://github.com/kosayoda/nvim-lightbulb/issues/20
 
-require("lspconfig").bashls.setup({
+vim.lsp.config("bashls", {
   capabilities = capabilities,
 })
+vim.lsp.enable("bashls")
